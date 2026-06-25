4413614b6b16303c55bb7d66f33a4b0670bf2d87
Bhagyashree Bamagod <bhagyashree@gmail.com>
2026-06-25 09:45:37 +0000

    Completed AI Fraud Detection API with Docker deployment

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import joblib
 import numpy as np
 
@@ -6,16 +7,33 @@ app = FastAPI()
 
 model = joblib.load("model/model.pkl")
 
+class Transaction(BaseModel):
+    user_id: int
+    amount: float
+    hour: int
+    location_match: int
+    is_night: int
+    user_avg_amount: float
+    amount_diff: float
+    high_amount: int
 
 @app.get("/")
 def home():
     return {"message": "Fraud Detection API Running"}
 
-
 @app.post("/predict")
-def predict(data: dict):
+def predict(data: Transaction):
 
-    features = np.array(data["features"]).reshape(1, -1)
+    features = np.array([[
+        data.user_id,
+        data.amount,
+        data.hour,
+        data.location_match,
+        data.is_night,
+        data.user_avg_amount,
+        data.amount_diff,
+        data.high_amount
+    ]])
 
     prediction = model.predict(features)[0]
     probability = model.predict_proba(features)[0][1]
